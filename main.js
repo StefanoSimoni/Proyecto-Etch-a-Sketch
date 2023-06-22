7372f7f23679360693bf625bb5ccf2fed7b9c8dd
Your Name <stefano.simoni.10@hotmail.com.ar>
2023-06-22 19:03:10 -0300

    Commit 2
    Creacion y ajustado de diferentes cuadriculas

--- main.js
+++ main.js
@@ -1,25 +1,38 @@
 const container = document.getElementById("container")
+const gridSize = document.getElementById("gridSize")
+const rainbow = document.getElementById("rainbow")
 
-const createGrid = () => {
-    for(let i = 0; i < 16; i++) {
+const createGrid = (size) => {
+    for(let i = 0; i < size; i++) {
         const content = document.createElement("div")
         content.setAttribute("id", "row" + i)
-        for(let j = 0; j < 16; j++) {
-            var contentGrid = document.createElement("div")
+        content.setAttribute("style", "display: flex; flex: 1; width: 400px")
+        for(let j = 0; j < size; j++) {
+            const contentGrid = document.createElement("div")
             contentGrid.setAttribute("id", i + "" + j)
-            contentGrid.setAttribute("style", "width: 40px; height: 20px; border: 1px solid black")
+            contentGrid.setAttribute("style", "flex: 1")
             content.appendChild(contentGrid)
             printGrid(contentGrid)
         }
         container.appendChild(content)
-        content.setAttribute("style", "display: flex")
     }
 }
 
 const printGrid = (box) => {
+    rainbow.addEventListener(("click"), () => {
+        box.addEventListener("dragover", () => {
+            box.style.backgroundColor = "rgb(random(255), random(255), randdom(255))"
+        })
+    })
     box.addEventListener("dragover", () => {
-        box.style.backgroundColor = "red"
+        box.style.backgroundColor = "rgb(255, 98, 71)"
     })
 }
 
-createGrid()
+gridSize.addEventListener(("click"), () => {
+    const size = prompt("Ingrese la cantidad de cuadrados por lado para la nueva cuadricula", "32")
+    container.innerHTML = ""
+    createGrid(size)
+})
+
+createGrid(16)
